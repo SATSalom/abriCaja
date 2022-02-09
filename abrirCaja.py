2f4b8677fc475ca4224de7b93bdd1af75d95b84d
Brian Passos <bri@npass.us>
2022-02-09 11:18:01 +0100

    Añadida primera revisón de lógica.

---
+++ abrirCaja.py
@@ -0,0 +1,13 @@
+# Código modificado usando como base el publicado en el siguiente enlace: https://stackoverflow.com/a/50737983
+
+import win32print # Librería necesaria para interactuar con las impresoras de Windows.
+
+def abrirCaja (impresora) :
+	impresoraHandler = win32print.OpenPrinter (impresora) # Iniciamos el punto a través del cual nos comunicaremos con la impresora.
+	comandoAbrirCaja = chr(27)+chr(112)+chr(0)+chr(25)+chr(250) # Serie de carácteres que la impresora interpreta como comando para abrir la caja.
+	win32print.StartDocPrinter (impresoraHandler, 1, ('Abrir caja.', None, 'RAW'))  # Iniciamos una petición de impresión con datos básico y descripcion del trabajo.
+	win32print.WritePrinter (impresoraHandler, comandoAbrirCaja.encode("utf-8")) # Pasamos el comando para abrir caja a la petición de impresión.
+	win32print.EndDocPrinter (impresoraHandler) # Finalizamos la petición de impresión.
+	win32print.ClosePrinter (impresoraHandler) # Cerramos la petición de impresión.
+
+abrirCaja ("Tickets") # Llamamos a la función.
